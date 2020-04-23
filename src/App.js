6944b3e4f9efe8b9f1c8531beaacdf1aace8b627
HyeonHak <52483468+HyeonHak@users.noreply.github.com>
2020-04-23 16:17:11 +0900

    jsx & prop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+
+function Food(props) {
+  return (
+    <div>
+      <h2>I like {props.name}</h2>
+      <h4>Rating : {props.rating} / 5.0</h4>
+      <img src={props.pictures} alt={props.name} />
+    </div>
+  );
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  pictures: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
+
+const foodILike = [
+  {
+    name: "Bank",
+    image: "./img/salad.JPG",
+    rating: 5,
+  },
+  {
+    name: "Salad",
+    image: "./img/salad.JPG",
+    rating: 5,
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>hello Black-Label!!!!!!!</h1>
+      {foodILike.map((dish) => (
+        <Food name={dish.name} pictures={dish.image} rating={dish.rating} />
+      ))}
     </div>
   );
 }
